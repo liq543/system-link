e8b919bb37f8e40141fb92c1dabefc34dd35c980
Griffin <liq543@gmail.com>
2024-01-04 09:33:55 -0600

    first

---
+++ system-link/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import LandingPage from './components/LandingPage'; // Ensure this path is correct
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
